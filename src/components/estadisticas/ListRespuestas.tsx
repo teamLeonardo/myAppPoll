068f739f9b48b23067aca736f27960a4fc139064
leonardo <leonardosm3.14@gmail.com>
2021-01-13 20:17:30 -0600

    go

--- src/components/estadisticas/ListRespuestas.tsx
+++ src/components/estadisticas/ListRespuestas.tsx
@@ -1,73 +1,79 @@
-import { IonCol, IonGrid, IonIcon, IonItem, IonLabel, IonList, IonRow } from '@ionic/react'
-import { closeCircle } from 'ionicons/icons'
-import React from 'react'
-
-export const ListRespuestas = ({ data, numQuery }: { data: any[], numQuery: number }) => {
-    return (
-        <IonList lines="full">
+import React, { useEffect, useState } from 'react'
+import { IonCard, IonCol, IonGrid, IonItem, IonList, IonListHeader, IonRow } from '@ionic/react'
+import DataTable from "react-data-table-component"
+const ExpanderItem = (props: any) => {
+    const [dataTrans, setDataTrans] = useState<any[]>([])
+    useEffect(() => {
+        let newDataArray = [];
+        for (let index = 1; index <= props.numQuery; index++) {
+            const pre = props.data[`pregunta${index}`]
+            const res = props.data[`respuesta${index}`]
+            newDataArray.push({ pre, res })
+        }
+        setDataTrans(newDataArray)
+    }, [props.data])
+    return <div>
+        <IonList inset>
+            <IonListHeader>
+                Preguantas
+            </IonListHeader>
             {
-                data.length > 0 && (
-                    <IonItem>
-                        <IonGrid>
-                            <IonRow>
-                                {
-                                    Array(numQuery).fill(0).map((nda, index) => {
-                                        let strinpr = "pregunta" + (index + 1)
-                                        return (
-                                            <IonCol key={index}>
-                                                <IonLabel>
-                                                    {data[0][strinpr]}
-                                                </IonLabel>
-                                            </IonCol>
-                                        )
-                                    })
-                                }
-
-                                <IonCol>
-                                    <IonLabel>
-                                        time
-                                    </IonLabel>
-                                </IonCol>
-                            </IonRow>
-                        </IonGrid>
-                    </IonItem>
-                )
-            }
-            {
-                data.map((value: any, index) => {
-                    let dt: any = new Date(value.time);
-                    let dformat = `${(dt.getMonth() + 1).toString().padStart(2, '0')}/${dt.getDate().toString().padStart(2, '0')}/${dt.getFullYear().toString().padStart(4, '0')} ${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}:${dt.getSeconds().toString().padStart(2, '0')}`;
-
-                    return <IonItem key={index}>
-                        <IonLabel>
-                            <IonGrid>
-                                <IonRow>
-                                    {
-                                        Array(numQuery).fill(0).map((nda, index) => {
-                                            let strinpr = "respuesta" + (index + 1)
-                                            console.log(strinpr);
-                                            
-                                            return (
-                                                <IonCol key={index}>
-                                                    <IonLabel>
-                                                        {data[0][strinpr]}
-                                                    </IonLabel>
-                                                </IonCol>
-                                            )
-                                        })
-                                    }
-                                    <IonCol>
-                                        {dformat && dformat}
-                                    </IonCol>
-                                    <IonCol>
-                                        <IonIcon icon={closeCircle} slot="end" />
-                                    </IonCol>
-                                </IonRow>
-                            </IonGrid>
-                        </IonLabel>
-                    </IonItem>
-                })
+                dataTrans.map((value, index) => <IonItem key={index}>
+                    <IonGrid>
+                        <IonRow>
+                            <IonCol>
+                                <strong>{value.pre}</strong>
+                            </IonCol>
+                            <IonCol>
+                                {value.res}
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </IonItem>)
             }
         </IonList>
+    </div>
+}
+
+
+export const ListRespuestas = ({ data, numQuery }: { data: any[], numQuery: number }) => {
+
+    const ConvertData = (value: number) => {
+        let dt: any = new Date(value);
+        return `${(dt.getMonth() + 1).toString().padStart(2, '0')}/${dt.getDate().toString().padStart(2, '0')}/${dt.getFullYear().toString().padStart(4, '0')} ${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}:${dt.getSeconds().toString().padStart(2, '0')}`;
+    }
+
+    const columns = [
+        {
+            name: "Name",
+            selector: "name",
+            sortable: true
+        },
+        {
+            name: "time",
+            selector: "time",
+            sortable: true
+        }
+
+    ];
+
+    return (
+        <IonGrid>
+            <IonRow>
+                <IonCol size="12">
+                    <IonCard >
+                        <DataTable
+                            columns={columns}
+                            data={data.map((value) => ({ ...value, time: ConvertData(value.time), name: value.name ? value.name : "anonymous" }), [])}
+                            expandableRows
+                            noHeader
+                            highlightOnHover
+                            defaultSortField="name"
+                            expandableRowsComponent={<ExpanderItem numQuery={numQuery} />}
+                        />
+                    </IonCard>
+                </IonCol>
+            </IonRow>
+        </IonGrid>
     )
 }
